ea6874be47df1d5188c3c27689d55cd5b6c15a2c
Joeliinus <joel-martin.riive@vikk.ee>
2022-12-11 13:19:20 +0200

    Create PyGame ÜL 2 Tekstide ja piltide kasutamine.py

---
+++ PyGame ÜL 2/PyGame ÜL 2 Tekstide ja piltide kasutamine.py
@@ -0,0 +1,34 @@
+import pygame
+pygame.init()
+
+# määrab ekraani suuruse ning paneb sellele nime
+screen = pygame.display.set_mode([640, 480])
+pygame.display.set_caption("2")
+
+# täidab ekraani pildiga, millest saab tagataust
+bgshop = pygame.image.load("bgshop.jpg")
+screen.blit(bgshop, [0, 0])
+
+# lisab müüa pildi ja muudab sele suurust ja asukohta
+seller = pygame.image.load("seller.png")
+seller = pygame.transform.scale(seller, [253, 307])
+screen.blit(seller, [105, 158])
+
+# lisab jutumulli pildi ja muudab selle suurust ja asukohta
+chat = pygame.image.load("chat.png")
+chat = pygame.transform.scale(chat, [257, 203])
+screen.blit(chat, [245, 66])
+
+# määrab teksti fondi suuruse, värvi, olemuse ja asukoha
+font = pygame.font.Font(pygame.font.match_font('comic sans'), 19)
+text = font.render("Tere, olen Joel Riive", True, [255, 255, 255])
+screen.blit(text, [280, 135])
+
+# sulgemine hiirega
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
