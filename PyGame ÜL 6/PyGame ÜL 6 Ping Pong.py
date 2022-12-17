49a6d5a9ec3a72d98cbc96b64b8c6734b40026c1
Joeliinus <joel-martin.riive@vikk.ee>
2022-12-17 22:31:36 +0200

    Update PyGame ÜL 6 Ping Pong.py

--- PyGame ÜL 6/PyGame ÜL 6 Ping Pong.py
+++ PyGame ÜL 6/PyGame ÜL 6 Ping Pong.py
@@ -14,7 +14,8 @@ alus liigub vasakule / paremale(vahetab suunda, kui puudub seinu)
 
 Kokkupõrke tuvastamine
 kui pall puutub alust siis muudab suunda.
-kui pall käitub kokkupuutel alusega imelikult, siis lisa tingimusse kontroll, et palli y - suund oleks suurem kui null(ballSpeedY > 0)
+kui pall käitub kokkupuutel alusega imelikult, siis lisa tingimusse kontroll,
+et palli y - suund oleks suurem kui null(ballSpeedY > 0)
 
 Boonus
 kui pall puudub alumist äärt, siis saab mängija negatiivse punkti
@@ -34,12 +35,38 @@ pygame.display.set_caption("Ping Pong")
 lBlue = [153, 204, 255]
 screen.fill(lBlue)
 
+pall = pygame.image.load("ball.png")
+pall = pygame.transform.scale(pall, [20, 20])
 alus = pygame.image.load("pad.png")
-alus = pygame.transform.rotate(alus, 180)
+alus = pygame.transform.scale(alus, [120, 20])
+posX = 0
+posY = screenY / 1.5
+speedX = 0.1
+posZ, posQ = 69, 289
+speedZ, speedQ = 0.2, 0.2
 
 # sulgemine hiirega
 while True:
     sisend = pygame.event.poll()
     if sisend.type == pygame.QUIT:
         sys.exit()
+
+    screen.blit(alus, (posX, posY))
+    screen.blit(pall, (posZ, posQ))
+
+    posX += speedX
+    posZ += speedZ
+    posQ += speedQ
+
+    # kui puudub ääri, siis muudab suunda
+    if posX > screenX-alus.get_rect().width or posX < 0:
+        speedX = -speedX
+
+    if posZ > screenX-pall.get_rect().width or posZ < 0:
+        speedZ = -speedZ
+
+    if posQ > screenY-pall.get_rect().width or posQ < 0:
+        speedQ = -speedQ
+
     pygame.display.flip()
+    screen.fill(lBlue)
