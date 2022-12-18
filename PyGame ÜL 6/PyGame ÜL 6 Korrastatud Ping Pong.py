2e06952517f8d9ab289a3eb0514f731a317d6250
Joeliinus <joel-martin.riive@vikk.ee>
2022-12-18 22:36:46 +0200

    Update PyGame ÜL 6 Korrastatud Ping Pong.py

--- PyGame ÜL 6/PyGame ÜL 6 Korrastatud Ping Pong.py
+++ PyGame ÜL 6/PyGame ÜL 6 Korrastatud Ping Pong.py
@@ -15,48 +15,54 @@ pygame.init()
 # EKRAANI SEADED
 screenX = 640   # laius on 640
 screenY = 480   # Kõrgus on 480
-ekraan = pygame.display.set_mode([screenX, screenY])   # ekraan avab akna määratud laiuse ja kõrgusega
+ekraan = pygame.display.set_mode([screenX, screenY])   # ekraan avab aknale määratud laiuse ja kõrgusega
 pygame.display.set_caption("Ping Pong")   # paneb ekraanile pealkirja
-lRed = [255, 204, 204]   # annad lRed-ile väärtuseks värvikoodi
-ekraan.fill(lRed)   # muudad taustavärvi lRed-iks
-skoor = 0   # määrad skoori, algväärtusega 0
-kell = pygame.time.Clock()   #
+lRed = [255, 204, 204]   # annab lRed-ile väärtuseks värvikoodi
+ekraan.fill(lRed)   # muudab taustavärvi lRed-iks
+skoor = 0   # määrab skoori, algväärtusega 0
+kell = pygame.time.Clock()   # loob kella objekti
 
-alus = pygame.image.load("pad.png")
-alus = pygame.transform.scale(alus, [120, 20])
-pall = pygame.image.load("ball.png")
-pall = pygame.transform.scale(pall, [20, 20])
-posX = 200
-posY = screenY / 1.5
-speedX = 0.2
-posZ, posQ = 99, 189
-speedZ, speedQ = 5, 5
-directionX, directionY = 0, 0
+# PILDID JA OMADUSED
+alus = pygame.image.load("pad.png")   # määrab alusele palgi pildi
+alus = pygame.transform.scale(alus, [120, 20])   # muudab aluse suurust
+pall = pygame.image.load("ball.png")   # määrab pallile palli pildi
+pall = pygame.transform.scale(pall, [20, 20])   # muudab palli suurust
+posX = 200   # määrab posX-i väärtuseks 200
+posY = screenY / 1.5   # määrab posY väärtuseks ekraani lauise jagatud 1.5-ga
+speedX = 0.2   # määrab speedX-i väärtuseks 0.2
+posZ, posQ = 99, 189   # määrab posZ-i väärtuseks 99 ja määrab posQ väärtuseks 189
+speedZ, speedQ = 5, 5   # määrab speedZ-i väärtuseks 5 ja speedQ väärtuseks 5
+directionX, directionY = 0, 0   # määrab directionX-i väärtuseks 0 ja directionY-i väärtuseks 0
 
-pygame.mixer.music.load('happy.mp3')
-pygame.mixer.music.play(0)
-pygame.mixer.music.set_volume(0.5)
-hit_sound = pygame.mixer.Sound('jump.ogg')
-end_sound = pygame.mixer.Sound('game-over.wav')
-log_hit = pygame.mixer.Sound('rock-hitting-log.wav')
+# HELI SEADED
+pygame.mixer.music.load('happy.mp3')   # laeb sisse muusika
+pygame.mixer.music.play(0)   # mängib sisse laetud muusikat
+pygame.mixer.music.set_volume(0.5)   # muudab heli tugevust
+hit_sound = pygame.mixer.Sound('jump.ogg')   # tekitad heliobjekti hit_sound
+end_sound = pygame.mixer.Sound('game-over.wav')   # tekitad heliobjekti end_sound
+log_hit = pygame.mixer.Sound('rock-hitting-log.wav')   # tekitad heliobjekti log_hit
 
+# AKENT LAHTI HOIDEV PÕHITSÜKKEL
 while True:
-    fps = kell.tick(60)
+    fps = kell.tick(60)   # lisab kaadrisageduse
 
+    # Sulgeb ekraani ristile vajutades
     sisend = pygame.event.poll()
     for sisend in pygame.event.get():
         if sisend.type == pygame.QUIT:
             sys.exit()
 
-    posZ += speedZ
-    posQ += speedQ
+    posZ += speedZ   # posZ suureneb speedZ võrra
+    posQ += speedQ   # posQ suureneb speedQ võrra
 
-    if posX > screenX-alus.get_rect().width or posX < 0:
-        speedX = -speedX
+    aluse_kast = ekraan.blit(alus, (posX, posY))   # määrab alusele tema pindala suuruse kasti ja algpositsioonid
+    pall_kast = ekraan.blit(pall, (posZ, posQ))   # määrab pallile tema pindala suuruse kasti ja algpositsioonid
 
+# PALLI KOKKUPUUDE ASJADEGA
+    # Kui posZ on suurem kui ekraani ja palli kasti laiuse vahe või kui posZ on väiksem kui null
     if posZ > screenX-pall.get_rect().width or posZ < 0:
-        speedZ = -speedZ
-        pygame.mixer.Sound.play(hit_sound)
+        speedZ = -speedZ   # siis speedZ muutub vastupidiseks
+        pygame.mixer.Sound.play(hit_sound)   # ja kostab
 
     if posQ < 0:
         speedQ = -speedQ
@@ -67,9 +73,6 @@ while True:
 
     if posQ > screenY-pall.get_rect().width:
         sys.exit()
-
-    aluse_kast = ekraan.blit(alus, (posX, posY))
-    pall_kast = ekraan.blit(pall, (posZ, posQ))
 
     font = pygame.font.Font(pygame.font.match_font('Impact'), 20)
     skoortekst = font.render("Punktid: " + str(skoor), True, [102, 0, 51])
