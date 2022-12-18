cc184dd279a3c53a1ab8a57362047dee53872250
Joeliinus <joel-martin.riive@vikk.ee>
2022-12-18 03:04:00 +0200

    Update PyGame ÜL 6 Ping Pong.py

--- PyGame ÜL 6/PyGame ÜL 6 Ping Pong.py
+++ PyGame ÜL 6/PyGame ÜL 6 Ping Pong.py
@@ -33,16 +33,17 @@ ekraan = pygame.display.set_mode([screenX, screenY])
 pygame.display.set_caption("Ping Pong")
 lBlue = [153, 204, 255]
 ekraan.fill(lBlue)
+skoor = 0
 
 pall = pygame.image.load("ball.png")
 pall = pygame.transform.scale(pall, [20, 20])
 alus = pygame.image.load("pad.png")
 alus = pygame.transform.scale(alus, [120, 20])
-posX = 0
+posX = 2
 posY = screenY / 1.5
-speedX = 0.1
+speedX = 0.3
 posZ, posQ = 69, 289
-speedZ, speedQ = 0.15, 0.15
+speedZ, speedQ = 0.5, 0.5
 
 while True:
     sisend = pygame.event.poll()
@@ -62,11 +63,23 @@ while True:
     if posQ > screenY-pall.get_rect().width or posQ < 0:
         speedQ = -speedQ
 
-    alus_kast = ekraan.blit(alus, (posX, posY))
+    aluse_kast = ekraan.blit(alus, (posX, posY))
     pall_kast = ekraan.blit(pall, (posZ, posQ))
 
-    if pall_kast.colliderect(alus_kast) and speedZ > 0:
+    font = pygame.font.Font(pygame.font.match_font('comic sans'), 16)
+    skoortekst = font.render("Punktid: " + str(skoor), True, [255, 255, 255])
+    ekraan.blit(skoortekst, [535, 20])
+
+    if pall_kast.colliderect(aluse_kast) and posQ > 0:
         speedQ = -speedQ
+        skoor += 4
+
+    if pall_kast.colliderect(aluse_kast) and speedQ > 0.2:
+        speedQ = -speedQ
+        skoor -= 4
+
+    if posQ > 460:
+        skoor -= 1
 
     pygame.display.flip()
     ekraan.fill(lBlue)
